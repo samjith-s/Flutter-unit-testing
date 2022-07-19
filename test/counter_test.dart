bd43700073a9adcb254d618222662deed8b71c0c
Samjith MK <Samjith.mk@myabpl.com>
2022-07-19 11:59:21 +0530

    counter test passed

---
+++ test/counter_test.dart
@@ -0,0 +1,28 @@
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unit_test_demo/counter.dart';
+
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
+}
